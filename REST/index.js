6813b3bf7ec64983bf63cb8a3833f60ceb93c2fb
itzmeak07 <ashishkr2612@gmail.com>
2025-08-27 18:28:58 +0530

    updating...

--- REST/index.js
+++ REST/index.js
@@ -1,3 +1,5 @@
+const { v4: uuidv4 } = require("uuid");
+const id = uuidv4();
 const express = require("express");
 const app = express();
 const port = 8080;
@@ -5,14 +7,40 @@ const path = require("path");
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set("public", express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-let posts = [{ username: "ashish kr", content: "this is my first post" }];
+let posts = [];
+
 app.get("/posts", (req, res) => {
-  res.render("index.ejs")
+  res.render("index.ejs", { posts });
+});git 
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = uuidv4();
+  posts.push({ id, username, content });
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((post) => post.id == id);
+  res.render("show.ejs", { post });
+});
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+  let newcontent = req.body.content;
+  console.log(newcontent);
+  let post = posts.find((post) => post.id == id);
+  post.content = newcontent;
+  console.log(post);
+  res.send("patch request is working");
 });
 app.listen(port, () => {
   console.log(` app is listening on port ${port}`);
 });
-
-
